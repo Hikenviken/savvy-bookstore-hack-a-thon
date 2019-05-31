798dea057745fd89fd0b27237664da006447db05
manavm1990 <manavm@visionwebsoft.com>
2019-05-31 16:16:24 -0500

    Refactor books to use 'map' and 'join.'

--- components/Book.js
+++ components/Book.js
@@ -1,9 +1,11 @@
+function buildSPHtml(sellingPoints) {
+    return sellingPoints.map(point => `<li>${point}</li>`).join("");
+}
+
 export default (state) => `
     <img src=${state.pictureUrl} alt="Photo wuz here">
-    <figcaption>${state.name} - ${state.author} - ${state.price}</figcaption>
+    <p>${state.name} - ${state.author} - ${state.price}</p>
     <ul>
-        <li>${state.sellingPoints[0]}</li>
-        <li>${state.sellingPoints[1]}</li>
-        <li>${state.sellingPoints[2]}</li>
+        ${buildSPHtml(state.sellingPoints)}
     </ul>
 `;

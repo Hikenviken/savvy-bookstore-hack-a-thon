85fa8748ff5162776b190e88304a291f07b206fd
manavm1990 <manavm@visionwebsoft.com>
2019-06-03 13:25:29 -0500

    Fix books property names to match API data 🔑s.

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ const states = {
     books: [
       {
         id: 1,
-        name: "Lasagna: A Retrospective",
-        author: "Garfield",
-        pictureUrl:
+        title: "Lasagna: A Retrospective",
+        creator: "Garfield",
+        image:
           "http://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg",
         price: 24,
-        sellingPoints: [
+        selling_points: [
           "Lasagna is delicious.",
           "The essential guide to Italian casseroles of all types.",
           "Real G's move silent, like Lasagna. -Lil Wayne"

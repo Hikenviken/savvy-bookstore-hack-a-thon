80779d5912f5048915089463fbf4608d80e0fe34
Paul Shannon <paulshannon36@gmail.com>
2019-07-22 20:41:42 -0500

    Add form js and function to export form

---
+++ form.js
@@ -0,0 +1,15 @@
+export default function(state) {
+  return `
+  <div class="form-stuff">
+
+<form action=" " method = "GET">
+  <input type="number" title=""/>
+  <input type="text"  title="name"/>
+  <input type="text" title="author" />
+  <input type="image" title="pictureUrl"/>
+  <input type="number" title="price"/>
+  <textarea name="sellingPoints">
+
+                  </textarea>
+`
+}

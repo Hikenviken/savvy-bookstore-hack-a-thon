81ef3f7aa4a0db1528506422f3fb90445cdfd045
manavm1990 <manavm@visionwebsoft.com>
2019-07-22 20:53:37 -0500

    Fix missing commas in state object.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const states = {
     {
       "id": 1,
       "name": "Lasagna: A Retrospective",
-      "author": "Garfield"
+      "author": "Garfield",
       "pictureUrl": "http://graphics8.nytimes.com/images/2015/10/15/dining/15RECIPE20DIN/15RECIPE20DIN-articleLarge.jpg",
       "price": 24,
       "sellingPoints": [
@@ -30,7 +30,7 @@ const states = {
     {
       "id": 2,
       "name": "Looking for JJ",
-      "author": "Anne Cassidy"
+      "author": "Anne Cassidy",
       "pictureUrl": "http://d202m5krfqbpi5.cloudfront.net/books/1328833113l/1522147.jpg",
       "price": 45,
       "sellingPoints": [
@@ -43,11 +43,11 @@ const states = {
     {
       "id": 3,
       "name": "The Alchemist",
-      "author": "Paulo Coelho"
+      "author": "Paulo Coelho",
       "pictureUrl": "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.buyoye.pk%2Fwp-content%2Fuploads%2F2017%2F04%2FThe-Alchemist-by-Paulo-Coelho.jpg&f=1",
       "price": 24,
       "sellingPoints": [
-          "My wrist deserve a shout out, 'I'm like what up wrist?' My stove deserve a shout out, 'I'm like what up stove"
+          "My wrist deserve a shout out, 'I'm like what up wrist?' My stove deserve a shout out, 'I'm like what up stove",
           "Amazing book, good read",
           "Moms Spaghetti"
       ]
@@ -56,11 +56,11 @@ const states = {
     {
       "id": 4,
       "name": "Tearing Down the Wall of Sound: The Rise and Fall of Phil Spector",
-      "author": "Mick Brown"
+      "author": "Mick Brown",
       "pictureUrl": "https://img.huffingtonpost.com/asset/5988b0111400001f00ecf8d9.jpeg?cache=bv2dgjc1xj&ops=crop_6_25_1165_1182,scalefit_720_noupscale",
       "price": 14.21,
       "sellingPoints": [
-          "Lasagna is delicious.This is a rip roaring read about Phil Spector the genius producer famous for his wall of sound and who made himself more famous than his artists who when he started out was a bit ego -centric"
+          "Lasagna is delicious.This is a rip roaring read about Phil Spector the genius producer famous for his wall of sound and who made himself more famous than his artists who when he started out was a bit ego -centric",
           "I loved the chapters about the creation of the Wall of Sound and Phil's big hits and famous failures, and the anecdotes detailing the music culture of the '60s and '70s.",
           "Wowee wow wow! -Chris Walken"
       ]

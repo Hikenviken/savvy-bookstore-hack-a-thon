73bb28f4a9595ce0560ea005277acd9e3cc171e8
Manav Misra <manavm@visionwebsoft.com>
2019-07-22 21:10:57 -0500

    Cleanup some form elements.

--- form.js
+++ form.js
@@ -1,23 +1,21 @@
 export default function(state) {
   return `
   <div class="form-stuff">
-<label for="book-query" >Book Query</label>
-<form action=" " method = "GET" title="book-query">
-  <label for="id"  >ID</label>
-  <input type="number" name="id" id="1" maxlength="5"
-  required />
-  <label for="name" >NAME: </label>
-  <input type="text"  name="name"/>
+  <form action=" " method = "GET" title="book-query">
+    <label for="name" >NAME: </label>
+    <input type="text"  name="name"/>
+  
   <label for="author" >AUTHOR: </label>
-  <input type="text" name="author" />
+    <input type="text" name="author" />
+  
   <label for="pictureUrl" >PICTURE URL: </label>
-  <input type="url" name="pictureUrl"/>
+    <input type="url" name="pictureUrl"/>
+  
   <label for="price" >PRICE: </label>
-  <input type="number" name="price"/>
-  <label for="sellingPoints" >SELLING POINTS: </label>
-  <textarea name="sellingPoints">
-
-                  </textarea>
+    <input type="number" name="price"/>
+  
+<label for="sellingPoints" >SELLING POINTS: </label>
+  <textarea name="sellingPoints"></textarea>
   </form>
   </div>
 `

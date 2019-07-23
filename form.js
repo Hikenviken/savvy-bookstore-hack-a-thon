d773779d621500df74590047413bfa34dfdf45af
Paul Shannon <paulshannon36@gmail.com>
2019-07-22 21:00:43 -0500

    Updates for form js. Added name attribute and label to each input tag.

--- form.js
+++ form.js
@@ -1,15 +1,23 @@
 export default function(state) {
   return `
   <div class="form-stuff">
-
-<form action=" " method = "GET">
-  <input type="number" title=""/>
-  <input type="text"  title="name"/>
-  <input type="text" title="author" />
-  <input type="image" title="pictureUrl"/>
-  <input type="number" title="price"/>
+<label for="book-query" >Book Query</label>
+<form action=" " method = "GET" title="book-query">
+  <label for="id"  >ID</label>
+  <input type="number" name="id" id="1" maxlength="5"
+  required />
+  <label for="name" >NAME: </label>
+  <input type="text"  name="name"/>
+  <label for="author" >AUTHOR: </label>
+  <input type="text" name="author" />
+  <label for="pictureUrl" >PICTURE URL: </label>
+  <input type="url" name="pictureUrl"/>
+  <label for="price" >PRICE: </label>
+  <input type="number" name="price"/>
+  <label for="sellingPoints" >SELLING POINTS: </label>
   <textarea name="sellingPoints">
 
                   </textarea>
+  </form>
 `
 }

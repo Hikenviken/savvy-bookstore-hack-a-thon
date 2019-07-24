9073c5a0c1fd79be0ff8292f76c4c8f95709887a
FloydCrenshaw <fmcrenshaw75@gmail.com>
2019-07-23 21:17:25 -0500

    successfully logged Navigation links

--- components/Navigation.js
+++ components/Navigation.js
@@ -4,7 +4,21 @@ export default function(state){
     <ul>
       <li>Books</li>
       <li>Albums</li>
-    </ul>
+    </ul>;
   </nav>
   `;
 }
+
+function generateNavHTML(links) {
+
+  console.log(links);
+}
+export default function(state) {
+  return `
+  <nav>
+          ${generateNavHTML(state.navigation)}
+  </nav>
+  `;
+}
+
+navigation: 

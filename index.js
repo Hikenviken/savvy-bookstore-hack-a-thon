31774dc0504e957200391a8d3821d0f258fc9c77
FloydCrenshaw <fmcrenshaw75@gmail.com>
2019-07-24 20:14:08 -0500

    Added click listeners to navigation and prevent default

--- index.js
+++ index.js
@@ -71,4 +71,11 @@ const states = {
 
 render(states);
 
+const navItems = document.querySelectorAll('nav li');
+
+navItems.forEach(function eventListenerAdder(navItem){
+    navItem.addEventListener('click' , function clickHandler(event){
+        event.preventDefault();
+    });
+});
 

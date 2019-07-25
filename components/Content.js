22fb580f33edc2be654d9f389dde4861daf1fe43
Manav Misra <manavm@visionwebsoft.com>
2019-07-24 20:31:38 -0500

    Clean Up Content

--- components/Content.js
+++ components/Content.js
@@ -1,22 +1,25 @@
 
  function generateBookHTML(books) {
-  let listHTML = '<ul>';
+  let listHTML = '';
   books.forEach(function(book) {
-    listHTML += `<div class="card"><li>${book.name}</li>
-    <li>${book.author}</li>
-    <li><img src=${book.pictureUrl}/></li>
-    <li>${book.price}</li>
-    <li>${book.sellingPoints}</li></div>`
+    listHTML += `
+    <div class="card">
+        <ul>
+            <li>${book.name}</li>
+            <li>${book.author}</li>
+            <li><img src=${book.pictureUrl}/></li>
+            <li>${book.price}</li>
+            <li>${book.sellingPoints}</li>
+        </ul>
+    </div>`
 
   });
-  listHTML += '</ul>'
   return listHTML;
 
 }
 
 
 export default function(state){
-console.log(state.books)
-return generateBookHTML(state.books);
+    return generateBookHTML(state.books);
 }
 
